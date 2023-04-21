ecfcc4ebc08782f82f0d91f160a1bd8849f5d61b
UncleGeorge <george.cosmoswill@gmail.com>
2023-04-21 14:03:19 -0400

    finish facebook login

--- FINAL_ZhangzhiWang/ViewController.swift
+++ FINAL_ZhangzhiWang/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  FINAL_ZhangzhiWang
-//
-//  Created by Zhangzhi Wang on 2023-04-18.
-//
 
 import UIKit
 import FacebookCore
@@ -11,20 +5,54 @@ import FacebookLogin
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fbButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
+    
+    private var isLoggedIn = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         adjustFontSize()
 
-        let fbLoginButton = FBLoginButton()
-//        loginButton.center = view.center
-        fbLoginButton.frame = loginButton.frame
-        
-        loginButton.removeFromSuperview()
+        let facebookLoginButton = FBLoginButton()
+        facebookLoginButton.delegate = self
 
-        view.addSubview(fbLoginButton)
+        facebookLoginButton.frame = fbButton.frame
+        
+        fbButton.removeFromSuperview()
+
+        view.addSubview(facebookLoginButton)
+        
+        if AccessToken.current != nil {
+            isLoggedIn = true
+        }
     }
+    
+    @IBAction func loginButtonClicked(_ sender: UIButton) {
+        if isLoggedIn {
+            performSegue(withIdentifier: "goToCalendar", sender: nil)
+        } else {
+            let alertController = UIAlertController(title: "Not Logged In", message: "Please log in first", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+}
+
+extension ViewController: LoginButtonDelegate{
+    func loginButton(_ loginButton: FBSDKLoginKit.FBLoginButton, didCompleteWith result: FBSDKLoginKit.LoginManagerLoginResult?, error: Error?) {
+        
+        if let error = error {
+            print("Error occurred during login: \(error.localizedDescription)")
+        } else if let result = result, !result.isCancelled {
+            isLoggedIn = true
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginKit.FBLoginButton) {
+        isLoggedIn = false
+    }
+    
     
 }
 
